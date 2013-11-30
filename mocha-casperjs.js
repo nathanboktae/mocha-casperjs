b79580b065eea98272ff17cb31b9e9a2203a1a05
Nathan Black <nathan@nathanblack.org>
2013-11-29 16:58:57 -0800

    don't call currentDone if not defined (eg when a test hasn't started yet)

--- mocha-casperjs.js
+++ mocha-casperjs.js
@@ -4,7 +4,9 @@ module.exports = function (Mocha, casper, utils) {
 
   failTest = function(error) {
     casper.unwait()
-    currentDone(error)
+    if (currentDone) {
+      currentDone(error)
+    }
   }
 
   Mocha.prototype.failCurrentTest = failTest;
